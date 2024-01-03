3b4ec35cae257aba1e45a61995d8bce63aa0fb3a
mkizka <g.mkizka@gmail.com>
2024-01-03 20:36:37 +0900

    Revert "update"
    This reverts commit 4522ce0ece902f1ace86d7a22b294ea3abbac1b9.

--- start.sh
+++ start.sh
@@ -2,8 +2,6 @@
 set -eu
 
 bundle exec rake db:migrate
-# https://github.com/mastodon/mastodon/issues/3676
-chown -hR 991.991 /mastodon/public/system
 # https://docs.joinmastodon.org/admin/setup/#admin-cli
 RAILS_ENV=production bin/tootctl accounts create \
   "$OWNER_USERNAME" \
