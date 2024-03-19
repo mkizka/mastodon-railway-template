a07654d2671059e0da5d4aeef4aec7bb2ec732e6
mkizka <g.mkizka@gmail.com>
2024-03-20 02:06:05 +0900

    fix for v4.2.8

--- start.sh
+++ start.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/bash
 set -eu
 
+export RAILS_ENV=production
+
 bundle exec rake db:migrate
 # https://docs.joinmastodon.org/admin/setup/#admin-cli
-RAILS_ENV=production bin/tootctl accounts create \
+bin/tootctl accounts create \
   "$OWNER_USERNAME" \
   --email "$OWNER_EMAIL" \
   --confirmed \
   --role Owner || true
+# https://github.com/mastodon/mastodon/releases/tag/v4.2.8
+bin/tootctl accounts modify --approve
+
 npx concurrently "bundle exec puma -C config/puma.rb" "bundle exec sidekiq"
